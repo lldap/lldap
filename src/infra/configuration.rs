acc80d21e5f51170de130af149ae27525091b125
Thomas Wickham <mackwic@gmail.com>
2021-03-02 20:30:43 +0100

    Add complex configuration

--- src/infra/configuration.rs
+++ src/infra/configuration.rs
@@ -1,6 +1,30 @@
-#[derive(Debug, Default)]
-pub struct Configuration;
+use anyhow::Result;
+use figment::{
+    providers::{Env, Format, Serialized, Toml},
+    Figment,
+};
+use serde::{Deserialize, Serialize};
 
-pub fn init() -> Configuration {
-    Configuration::default()
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Configuration {
+    secret_pepper: String,
+    some_text: String,
+}
+
+impl Default for Configuration {
+    fn default() -> Self {
+        Configuration {
+            secret_pepper: String::from("secretsecretpepper"),
+            some_text: String::new(),
+        }
+    }
+}
+
+pub fn init() -> Result<Configuration> {
+    let config: Configuration = Figment::from(Serialized::defaults(Configuration::default()))
+        .merge(Toml::file("lldap_config.toml"))
+        .merge(Env::prefixed("LLDAP_"))
+        .extract()?;
+
+    Ok(config)
 }
