5615ef8e1f72d9eefda04108be67143cc7643b41
Valentin Tolmer <valentin@tolmer.fr>
2021-05-11 09:54:54 +0200

    Split the user table into a sub-component

--- app/src/app.rs
+++ app/src/app.rs
@@ -1,60 +1,20 @@
-use crate::api::HostService;
-use anyhow::Error;
-use lldap_model::*;
-use yew::format::Json;
+use crate::user_table::UserTable;
 use yew::prelude::*;
-use yew::services::{fetch::FetchTask, ConsoleService};
 
-pub struct App {
-    link: ComponentLink<Self>,
-    ipservice: HostService,
-    task: Option<FetchTask>,
-    users: Option<Vec<User>>,
-}
+pub struct App {}
 
-pub enum Msg {
-    //BindRequest(BindRequest),
-    ListUsersRequest(ListUsersRequest),
-    ListUsersResponse(Result<Vec<User>, Error>),
-}
+pub enum Msg {}
 
 impl Component for App {
     type Message = Msg;
     type Properties = ();
 
-    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
-        App {
-            link: link.clone(),
-            ipservice: HostService::default(),
-            task: None,
-            users: None,
-        }
+    fn create(_: Self::Properties, _link: ComponentLink<Self>) -> Self {
+        App {}
     }
 
-    fn update(&mut self, msg: Self::Message) -> ShouldRender {
-        match msg {
-            Msg::ListUsersRequest(req) => {
-                match self
-                    .ipservice
-                    .list_users(req, self.link.callback(Msg::ListUsersResponse))
-                {
-                    Ok(task) => self.task = Some(task),
-                    Err(e) => {
-                        self.task = None;
-                        ConsoleService::log(format!("Error trying to fetch users: {}", e).as_str())
-                    }
-                }
-            }
-            Msg::ListUsersResponse(Ok(users)) => {
-                self.users = Some(users);
-                ConsoleService::log(format!("Response: {:?}", Json(&self.users)).as_str());
-            }
-            Msg::ListUsersResponse(Err(e)) => {
-                self.task = None;
-                ConsoleService::log(format!("Error listing users: {}", e).as_str())
-            }
-        }
-        true
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        false
     }
 
     fn change(&mut self, _: Self::Properties) -> ShouldRender {
@@ -65,8 +25,7 @@ impl Component for App {
         html! {
             <div>
                 <h1>{ "LLDAP" }</h1>
-                <button onclick=self.link.callback(|_| Msg::ListUsersRequest(ListUsersRequest{filters: None}))>{ "Fetch users" }</button>
-                <p>{ format!("Users: {:?}", &self.users) }</p>
+                <UserTable />
             </div>
         }
     }
---
+++ app/src/user_table.rs
@@ -0,0 +1,79 @@
+use crate::api::HostService;
+use anyhow::{anyhow, Result};
+use lldap_model::*;
+use yew::format::Json;
+use yew::prelude::*;
+use yew::services::{fetch::FetchTask, ConsoleService};
+
+pub struct UserTable {
+    link: ComponentLink<Self>,
+    ipservice: HostService,
+    task: Option<FetchTask>,
+    users: Option<Result<Vec<User>>>,
+}
+
+pub enum Msg {
+    ListUsersResponse(Result<Vec<User>>),
+}
+
+impl UserTable {
+    fn get_users(&mut self, req: ListUsersRequest) {
+        match self
+            .ipservice
+            .list_users(req, self.link.callback(Msg::ListUsersResponse))
+        {
+            Ok(task) => self.task = Some(task),
+            Err(e) => {
+                self.task = None;
+                ConsoleService::log(format!("Error trying to fetch users: {}", e).as_str())
+            }
+        };
+    }
+}
+
+impl Component for UserTable {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        let mut table = UserTable {
+            link: link.clone(),
+            ipservice: HostService::default(),
+            task: None,
+            users: None,
+        };
+        table.get_users(ListUsersRequest { filters: None });
+        table
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::ListUsersResponse(Ok(users)) => {
+                self.users = Some(Ok(users));
+                ConsoleService::log(format!("Response: {:?}", Json(&self.users)).as_str());
+                true
+            }
+            Msg::ListUsersResponse(Err(e)) => {
+                self.task = None;
+                self.users = Some(Err(anyhow!("Error listing users: {}", e)));
+                true
+            }
+        }
+    }
+
+    fn change(&mut self, _: Self::Properties) -> ShouldRender {
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <p>{
+                match &self.users {
+                     None => "Loading...".to_string(),
+                     Some(Ok(users)) => format!("Users: {:?}", &users),
+                     Some(Err(e)) => e.to_string(),
+                }
+            }</p>
+        }
+    }
+}
