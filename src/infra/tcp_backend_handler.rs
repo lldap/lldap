305fabe440ec1db91ff2a189cff5273ea8ca3d78
Marcelin Dupraz <marcelin.dupraz@gmail.com>
2021-05-30 21:11:13 +0200

    Implementation of the delete_user_request. Tests are barebones and are dependant of the list_users_request (delete_user_request_test will fail if list_users_request fails)

--- src/infra/tcp_backend_handler.rs
+++ src/infra/tcp_backend_handler.rs
@@ -28,6 +28,7 @@ mockall::mock! {
         async fn list_groups(&self) -> DomainResult<Vec<Group>>;
         async fn get_user_groups(&self, user: String) -> DomainResult<HashSet<String>>;
         async fn create_user(&self, request: CreateUserRequest) -> DomainResult<()>;
+        async fn delete_user(&self, request: DeleteUserRequest) -> DomainResult<()>;
         async fn create_group(&self, request: CreateGroupRequest) -> DomainResult<i32>;
         async fn add_user_to_group(&self, request: AddUserToGroupRequest) -> DomainResult<()>;
     }
