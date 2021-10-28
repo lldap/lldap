0f8f929c6d91348ef4316a2c66b761d9782280d9
Valentin Tolmer <valentin.tolmer@gmail.com>
2021-10-29 01:04:09 +0900

    ldap: Add support for password modify extension
    This allows other systems (e.g. Authelia) to reset passwords for users.

--- server/src/domain/opaque_handler.rs
+++ server/src/domain/opaque_handler.rs
@@ -28,9 +28,18 @@ mockall::mock! {
     }
     #[async_trait]
     impl OpaqueHandler for TestOpaqueHandler {
-    async fn login_start(&self, request: login::ClientLoginStartRequest) -> Result<login::ServerLoginStartResponse>;
-    async fn login_finish(&self, request: login::ClientLoginFinishRequest ) -> Result<String>;
-    async fn registration_start(&self, request: registration::ClientRegistrationStartRequest) -> Result<registration::ServerRegistrationStartResponse>;
-    async fn registration_finish(&self, request: registration::ClientRegistrationFinishRequest ) -> Result<()>;
+        async fn login_start(
+            &self,
+            request: login::ClientLoginStartRequest
+        ) -> Result<login::ServerLoginStartResponse>;
+        async fn login_finish(&self, request: login::ClientLoginFinishRequest ) -> Result<String>;
+        async fn registration_start(
+            &self,
+            request: registration::ClientRegistrationStartRequest
+        ) -> Result<registration::ServerRegistrationStartResponse>;
+        async fn registration_finish(
+            &self,
+            request: registration::ClientRegistrationFinishRequest
+        ) -> Result<()>;
     }
 }
