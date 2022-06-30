2f1bf87102d36fcdd24d24df8352f7b9a8f7dcc8
Valentin Tolmer <valentin@tolmer.fr>
2022-06-30 09:27:51 +0200

    app: propagate change events

--- app/src/components/create_group.rs
+++ app/src/components/create_group.rs
@@ -92,8 +92,8 @@ impl Component for CreateGroupForm {
         CommonComponentParts::<Self>::update(self, msg)
     }
 
-    fn change(&mut self, _: Self::Properties) -> ShouldRender {
-        false
+    fn change(&mut self, props: Self::Properties) -> ShouldRender {
+        self.common.change(props)
     }
 
     fn view(&self) -> Html {
--- app/src/components/reset_password_step2.rs
+++ app/src/components/reset_password_step2.rs
@@ -145,8 +145,8 @@ impl Component for ResetPasswordStep2Form {
         CommonComponentParts::<Self>::update(self, msg)
     }
 
-    fn change(&mut self, _: Self::Properties) -> ShouldRender {
-        false
+    fn change(&mut self, props: Self::Properties) -> ShouldRender {
+        self.common.change(props)
     }
 
     fn view(&self) -> Html {
