09d85c5e7898ee199902e4f859e78036956366aa
Kumpelinus <kumpelinus@jat.de>
2025-08-27 23:45:08 +0200

    Remove let chain

--- crates/ldap/src/search.rs
+++ crates/ldap/src/search.rs
@@ -293,8 +293,7 @@ pub fn make_ldap_subschema_entry(schema: PublicSchema) -> LdapOp {
 pub(crate) fn is_root_dse_request(request: &LdapSearchRequest) -> bool {
     if request.base.is_empty()
         && request.scope == LdapSearchScope::Base
-        && let LdapFilter::Present(attribute) = &request.filter
-        && attribute.eq_ignore_ascii_case("objectclass")
+        && matches!(&request.filter, LdapFilter::Present(attribute) if attribute.eq_ignore_ascii_case("objectclass"))
     {
         return true;
     }
