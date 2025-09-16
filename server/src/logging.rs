095c3252c351df5577fbf011aa8350cee32fa21a
vic1707 <28602203+vic1707@users.noreply.github.com>
2025-09-16 11:06:05 +0200

    add new `LLDAP_LOCAL_TZ_LOG` env

--- server/src/logging.rs
+++ server/src/logging.rs
@@ -6,7 +6,9 @@ use actix_web::{
 use std::env;
 use tracing::{Span, debug, error};
 use tracing_actix_web::RootSpanBuilder;
-use tracing_subscriber::{filter::EnvFilter, layer::SubscriberExt, util::SubscriberInitExt};
+use tracing_subscriber::{
+    filter::EnvFilter, fmt::time::ChronoLocal, layer::SubscriberExt, util::SubscriberInitExt,
+};
 
 /// We will define a custom root span builder to capture additional fields, specific
 /// to our application, on top of the ones provided by `DefaultRootSpanBuilder` out of the box.
@@ -43,13 +45,20 @@ pub fn init(config: &Configuration) -> anyhow::Result<()> {
             "sqlx=warn,reqwest=warn,info"
         })
     });
-    let registry = tracing_subscriber::registry()
-        .with(env_filter)
-        .with(tracing_forest::ForestLayer::default());
-    if env::var("LLDAP_RAW_LOG").is_ok() {
+    let registry = tracing_subscriber::registry().with(env_filter);
+
+    let raw_logs = env::var("LLDAP_RAW_LOG").is_ok();
+    let local_tz = env::var("LLDAP_LOCAL_TZ_LOG").is_ok();
+
+    if local_tz {
+        registry
+            .with(tracing_subscriber::fmt::layer().with_timer(ChronoLocal::rfc_3339()))
+            .init();
+    } else if raw_logs {
         registry.with(tracing_subscriber::fmt::layer()).init();
     } else {
-        registry.init();
+        registry.with(tracing_forest::ForestLayer::default()).init();
     }
+
     Ok(())
 }
